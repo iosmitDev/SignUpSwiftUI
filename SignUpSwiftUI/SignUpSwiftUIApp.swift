1885d685a939bce05838de16f05d42397757eb72
miteshpate-admin <Mitesh.imply@gmail.com>
2024-11-10 00:53:47 +0530

    Initial Commit

---
+++ SignUpSwiftUI/SignUpSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpSwiftUIApp.swift
+//  SignUpSwiftUI
+//
+//  Created by MiteshKumar Patel on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SignUpSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
