c6747a05219b4fb8a609d2fd867acdfc3aa6b686
miteshpate-admin <Mitesh.imply@gmail.com>
2024-11-11 18:35:47 +0530

    New Folder Updated and new SVN file created On Feature branch

---
+++ SignUpSwiftUI/Views/Login/SVNNewFile/NewSVNFile.swift
@@ -0,0 +1,18 @@
+//
+//  NewSVNFIle.swift
+//  SignUpSwiftUI
+//
+//  Created by MiteshKumar Patel on 11/11/24.
+//
+
+import SwiftUI
+
+struct NewSVNFIle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewSVNFIle()
+}
