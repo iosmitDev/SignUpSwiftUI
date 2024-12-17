84d460a5acbd3954b5ddf8d3987150f181c5f46b
iosmitDev <mitesh.imply@gmail.com>
2024-12-17 18:50:41 +0530

    Add MockChartData file

--- SignUpSwiftUI/Views/Charts/ChartView.swift
+++ SignUpSwiftUI/Views/Charts/ChartView.swift
@@ -10,7 +10,16 @@ import Charts
 
 struct ChartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Chart(MockData) { item in
+                    
+                    SectorMark(angle: .value("Label", item), innerRadius: .ratio(0.5), angularInset: 2)
+                        .foregroundStyle(item.color)
+                }
+            }
+            .navigationTitle("Pie Charts")
+        }
     }
 }
 
---
+++ SignUpSwiftUI/Views/Charts/MockChartData.swift
@@ -0,0 +1,41 @@
+//
+//  MockChartData.swift
+//  SignUpSwiftUI
+//
+//  Created by MiteshKumar Patel on 17/12/24.
+//
+
+import Foundation
+import SwiftUI
+import Charts
+
+//We need to create chart so we need id, Color and plottableData
+struct MockChartData: Identifiable, Plottable {
+    
+    var id = UUID().uuidString
+    let primitivePlottable: Int
+    let color: Color
+    
+    init?(primitivePlottable: Int) {
+        self.primitivePlottable = primitivePlottable
+        self.color = .blue
+    }
+    
+    init?(primitivePlottable: Int, color: Color) {
+        self.primitivePlottable = primitivePlottable
+        self.color = color
+    }
+    
+}
+
+let MockData: [MockChartData] = [
+    MockChartData(primitivePlottable: 12, color: .red)!,
+    MockChartData(primitivePlottable: 6, color: .blue)!,
+    MockChartData(primitivePlottable: 32, color: .orange)!,
+    MockChartData(primitivePlottable: 11, color: .green)!,
+    MockChartData(primitivePlottable: 38, color: .purple)!,
+    MockChartData(primitivePlottable: 18, color: .pink)!,
+    MockChartData(primitivePlottable: 7, color: .yellow)!
+]
+
+
