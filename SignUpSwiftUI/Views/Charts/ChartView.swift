ad0bbe3a486f9ef7229de80a105bce8badf7b936
iosmitDev <mitesh.imply@gmail.com>
2024-12-17 18:57:12 +0530

    I fixed inset height of donut becuase of very big space between different colors

--- SignUpSwiftUI/Views/Charts/ChartView.swift
+++ SignUpSwiftUI/Views/Charts/ChartView.swift
@@ -14,7 +14,7 @@ struct ChartView: View {
             VStack {
                 Chart(MockData) { item in
                     
-                    SectorMark(angle: .value("Label", item), innerRadius: .ratio(0.5), angularInset: 2)
+                    SectorMark(angle: .value("Label", item), innerRadius: .ratio(0.5), angularInset: 1)
                         .foregroundStyle(item.color)
                 }
             }
