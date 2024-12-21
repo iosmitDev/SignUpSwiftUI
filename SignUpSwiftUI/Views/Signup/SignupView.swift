f4977f8c760ef1596837b28b1ba57312540a6993
iosmitDev <mitesh.imply@gmail.com>
2024-12-21 19:55:12 +0530

    [Feature] add bolt icon

--- SignUpSwiftUI/Views/Signup/SignupView.swift
+++ SignUpSwiftUI/Views/Signup/SignupView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct SignupView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "bolt")
+            
+        }
     }
 }
 
