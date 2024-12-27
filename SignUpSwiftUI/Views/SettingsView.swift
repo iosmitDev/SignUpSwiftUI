23cfc903940bfad7e8099326e4183fb33f64ffb8
iosmitDev <mitesh.imply@gmail.com>
2024-12-28 03:35:11 +0530

    [Feature] Add SettingView

---
+++ SignUpSwiftUI/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SignUpSwiftUI
+//
+//  Created by MiteshKumar Patel on 28/12/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Mitesh Setting View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
