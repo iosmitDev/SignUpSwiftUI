c859794c019817b24eb4fa41d06fa7b8948abe31
iosmitDev <mitesh.imply@gmail.com>
2025-01-06 18:42:39 +0530

    [Feature] Finish feature

--- SignUpSwiftUI/Views/Charts/ChartView.swift
+++ SignUpSwiftUI/Views/Charts/ChartView.swift
@@ -71,7 +71,7 @@ struct ChartView: View {
                     .cornerRadius(20)
             }
             .padding()
-            .navigationTitle("Pie Charts.")
+            .navigationTitle("Pie Charts..")
             Spacer()
         }
         
